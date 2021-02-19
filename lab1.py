dfa6677b8f151addb5c12c5d52fda0d1c53d5bbb
nazarlonevskyi <nazar.lonevskyi.itis.2018@lpnu.ua>
2021-02-19 13:40:41 +0200

    add file

---
+++ lab1.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+
+print("HELLo, World!")
+print("Please, type your name")
+name = input()
+print("HELLo," + name)
+print("Latters in your name=" + str(len(name)))
+print("Please type your date of birthday ib YYYY.MM.DD:")
+count = input()
